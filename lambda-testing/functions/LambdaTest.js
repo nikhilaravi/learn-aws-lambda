4191fe00aabc1d0ed5ae24e344878ad23b804318
Nikhila Ravi <nikkiravi92@gmail.com>
2016-01-27 13:10:36 +0000

    Adds console log to lambda func

--- lambda-testing/functions/LambdaTest.js
+++ lambda-testing/functions/LambdaTest.js
@@ -2,5 +2,6 @@ console.log('Loading event');
 console.log('Loading event again');
 
 exports.handler = function(event, context) {
+    console.log("EVENT", event)
     context.succeed(event.key1);  // SUCCESS with message
 };
