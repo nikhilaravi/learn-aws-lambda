2faf7c923f67d88f25ab26e84a71756ce114c923
Nikhila Ravi <nikkiravi92@gmail.com>
2016-01-27 13:05:31 +0000

    Modifies folder structure

--- LambdaTest.js
+++
@@ -1,7 +0,0 @@
-console.log('Loading event');
-console.log('Loading event again');
-
-exports.handler = function(event, context) {
-    console.log("EVENT", event.key1);
-    context.succeed(event.key1);  // SUCCESS with message
-};
