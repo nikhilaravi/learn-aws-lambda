6f3ae199ecb73eace3f4acdf56e47cffaafe5f43
Nikhila Ravi <nikkiravi92@gmail.com>
2016-01-27 11:54:49 +0000

    Modifies lambda function

--- LambdaTest.js
+++ LambdaTest.js
@@ -2,5 +2,6 @@ console.log('Loading event');
 console.log('Loading event again');
 
 exports.handler = function(event, context) {
+    console.log("EVENT", event.key1);
     context.succeed(event.key1);  // SUCCESS with message
 };
